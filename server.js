45fbc55bb4316dc8b8e7feff6c6604608c2f06ea
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-10-29 18:34:20 +0530

    edit

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use((err, req, res, next) => {
 
 // ✅ Start server
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
 
