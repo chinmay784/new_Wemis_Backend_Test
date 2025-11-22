b0cdb04df0b1834e93fdee28e117dd0458906926
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-11-22 15:43:24 +0530

    editrd

--- database/Db.js
+++ database/Db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 exports.connectToDatabase = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL);
+        await mongoose.connect("mongodb+srv://chinmaypuhan420:h6ZifhrYLbD0Ou4L@wemis.qcsdnfy.mongodb.net/wemis");
         console.log('Database connected successfully');
     } catch (error) {
         console.error('Database connection failed:', error);
