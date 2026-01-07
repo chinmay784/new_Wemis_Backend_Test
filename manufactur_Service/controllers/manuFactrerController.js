917525d1dc29d9e768eba41a742757683f4e7525
chinmaypuhan <chinmaypuhan420@gmail.com>
2026-01-07 10:47:22 +0530

    edited

--- manufactur_Service/controllers/manuFactrerController.js
+++ manufactur_Service/controllers/manuFactrerController.js
@@ -4512,18 +4512,18 @@ exports.liveTrackingAllDevices = async (req, res) => {
 
 
 
-const formatDateTime = (timestamp) => {
-    if (!timestamp) return null;
-    return new Date(timestamp).toLocaleString("en-IN", {
-        day: "2-digit",
-        month: "short",
-        year: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-        hour12: true
-    });
-};
+// const formatDateTime = (timestamp) => {
+//     if (!timestamp) return null;
+//     return new Date(timestamp).toLocaleString("en-IN", {
+//         day: "2-digit",
+//         month: "short",
+//         year: "numeric",
+//         hour: "2-digit",
+//         minute: "2-digit",
+//         second: "2-digit",
+//         hour12: true
+//     });
+// };
 
 exports.liveTrackingAllDevices = async (req, res) => {
     try {
