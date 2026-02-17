5b52ce20eb9e36a92290200e2c7253f2829c69a7
chinmaypuhan <chinmaypuhan420@gmail.com>
2026-02-17 15:05:04 +0530

    edit

--- database/Db.js
+++ database/Db.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 
 exports.connectToDatabase = async () => {
     try {
-        await mongoose.connect("mongodb+srv://chinmaypuhan420:h6ZifhrYLbD0Ou4L@wemis.qcsdnfy.mongodb.net/wemis");
+        await mongoose.connect("mongodb+srv://admin_db_user:qRAFrILPKo8LbL2m@cluster0.fn0tntx.mongodb.net/wemis");
         console.log('Database connected successfully');
     } catch (error) {
         console.error('Database connection failed:', error);
         process.exit(1); // Exit the process with failure
     }
 };
+// mongodb+srv://chinmaypuhan420:h6ZifhrYLbD0Ou4L@wemis.qcsdnfy.mongodb.net
