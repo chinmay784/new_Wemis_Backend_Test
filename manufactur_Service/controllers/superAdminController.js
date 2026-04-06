7b79774961a4ca35b6811f74c63fc41849f2a1c5
chinmaypuhan <chinmaypuhan420@gmail.com>
2026-04-06 15:03:21 +0530

    Edit::

--- manufactur_Service/controllers/superAdminController.js
+++ manufactur_Service/controllers/superAdminController.js
@@ -90,7 +90,7 @@ exports.login = async (req, res) => {
             });
         }
 
-        const token = jwt.sign({ userId: user._id }, "wemis", { expiresIn: "5d" })
+        const token = jwt.sign({ userId: user._id }, "wemis", { expiresIn: "1h" })
 
         res.status(200).json({
             message: 'Super admin logged in successfully',
