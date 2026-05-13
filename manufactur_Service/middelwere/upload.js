4621bb758dcc952ec515f508da3ac3ac2a99c265
traxoindia <admin@traxoindia.in>
2026-05-13 14:48:53 +0530

    edit

--- manufactur_Service/middelwere/upload.js
+++ manufactur_Service/middelwere/upload.js
@@ -1,18 +1,34 @@
+
+
+
+
+
 // // middleware/upload.js
+
 // const multer = require("multer");
 // const path = require("path");
+// const fs = require("fs");
+
+// // ✅ Create uploads folder automatically
+// if (!fs.existsSync("uploads")) {
+//     fs.mkdirSync("uploads");
+// }
 
 // const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
+
+//     destination: function (req, file, cb) {
+
+//         cb(null, "uploads/");
+//     },
+
+//     filename: function (req, file, cb) {
+
+//         cb(null, Date.now() + path.extname(file.originalname));
+//     },
 // });
 
 // const Upload = multer({
-//   storage: storage,
+//     storage: storage,
 // });
 
 // module.exports = Upload;
@@ -20,22 +36,24 @@
 
 
 
-// middleware/upload.js
+
 
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
 // ✅ Create uploads folder automatically
-if (!fs.existsSync("uploads")) {
-    fs.mkdirSync("uploads");
+const uploadPath = "uploads/";
+
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
 }
 
 const storage = multer.diskStorage({
 
     destination: function (req, file, cb) {
 
-        cb(null, "uploads/");
+        cb(null, uploadPath);
     },
 
     filename: function (req, file, cb) {
